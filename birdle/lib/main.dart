64945e689d4896f7a8d95a67d616c6ef5087ef74
Jeffi V Binu <jeffibinu@gmail.com>
2026-04-18 00:42:22 +0530

    Adding the game logic code and updating its structure in the README file.

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() { // This is the entry point to the dart program
+  runApp(const MainApp()); // The runApp is a part of the flutter SDK and it takes a widget and makes it the root of the widget tree. In this case, we are passing an instance of MainApp.
 }
 
 class MainApp extends StatelessWidget {
